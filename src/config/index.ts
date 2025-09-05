760a0af44f56f0b0c00f80a06cc2bac5963bdb0d
Hosnain Rafi <rhosnain@gmail.com>
2025-09-05 22:19:36 +0600

    Update config for Vercel deployment

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,15 @@
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+// dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: path.join(process.cwd(), ".env") });
+}
 
 export default {
-  env: process.env.NODE_ENV,
-  port: process.env.PORT,
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 5000,
   database_url: process.env.DATABASE_URL,
   client_origin: process.env.CLIENT_ORIGIN, // Make sure to export this
 };
